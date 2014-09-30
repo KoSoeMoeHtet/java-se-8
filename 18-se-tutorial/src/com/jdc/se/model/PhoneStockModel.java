bb7f62b70eeb6ace5fd2cbb65191da841d84abd2
minlwin <lwin.zawmin@gmail.com>
2014-09-30 23:00:30 +0630

    Add implementation of StockModel

--- 18-se-tutorial/src/com/jdc/se/model/PhoneStockModel.java
+++ 18-se-tutorial/src/com/jdc/se/model/PhoneStockModel.java
@@ -1,11 +1,26 @@
 package com.jdc.se.model;
 
+import java.sql.SQLException;
+import java.util.Map;
+
 import com.jdc.se.entity.Phone_stock;
 
-public class PhoneStockModel extends CommonModel<Phone_stock> {
+public class PhoneStockModel extends CommonModel<Phone_stock> implements StockModel<Phone_stock>{
 
 	public PhoneStockModel() {
 		super(Phone_stock.class);
+	}
+
+	@Override
+	public void addStock(Map<String, Object> param) throws SQLException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateStock(Map<String, Object> param) throws SQLException {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
