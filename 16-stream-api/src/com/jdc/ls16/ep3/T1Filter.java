c076f374a3e99a52f156c78cdf3e332f988cc163
minlwin <lwin.zawmin@gmail.com>
2017-04-29 13:36:28 +0630

    Change Class Path

--- 16-stream-api/src/com/jdc/ls16/ep3/T1Filter.java
+++ 16-stream-api/src/com/jdc/ls16/ep3/T1Filter.java
@@ -1,10 +1,13 @@
 package com.jdc.ls16.ep3;
 
 
+
 public class T1Filter {
 
 	public static void main(String[] args) {
-		T0Common.getUser().stream().filter(a -> a.getAge() > 20)
+		
+		T0Common.getUser().stream()
+				.filter(u -> u.getAge() > 20)
 				.map(a -> a.getName()).forEach(System.out::println);
 	}
 }
